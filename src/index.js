80031b10adcd426a0a218d7fb7f44d0272e3e010
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-10-07 23:24:13 +0530

    Completed 'next 7 days' Section design and functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "./styles.css";
 import rainsvg from "./images/rain.svg";
+import cloudysvg from "./images/cloudy.svg";
+import snowsvg from "./images/snow.svg";
+import windysvg from "./images/windy.svg";
+import cleardaysvg from "./images/clear-day.svg";
+import clearnightsvg from "./images/clear-night.svg";
+import fogsvg from "./images/fog.svg";
+import partlycloudydaysvg from "./images/partly-cloudy-day.svg";
+import partlycloudynightsvg from "./images/partly-cloudy-night.svg";
+import precipitationsvg from "./images/precipitation.svg";
 
-
-let tempUnit='c';
+let tempUnit='c', speedUnit='km/hr';
 
 async function getWeather(location="vellore") {
   let weatherData = await fetch(
@@ -10,12 +18,13 @@ async function getWeather(location="vellore") {
   );
   let weatherJSON = await weatherData.json();
   let processedWeather= processWeatherData(weatherJSON);
-  displayWeatherData(processedWeather);
+  displayCurrentConditions(processedWeather);
+  displayWeekWeather(processedWeather);
   console.log(processedWeather);
 }
 
 function processWeatherData(weatherJSON){
-  console.log(weatherJSON);
+  //console.log(weatherJSON);
   let date=new Date(weatherJSON.days[0].datetime);
   let formattedDate=date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
   return {
@@ -40,7 +49,7 @@ function processWeatherData(weatherJSON){
   }
 }
 
-function displayWeatherData(processedWeather){
+function displayCurrentConditions(processedWeather){
   const currentPrimary=document.querySelector('.currentweather .primary');
   const currentSecondary=document.querySelector('.currentweather .secondary');
   currentPrimary.querySelector('.location').textContent=processedWeather.address;
@@ -49,26 +58,86 @@ function displayWeatherData(processedWeather){
   currentPrimary.querySelector('.date').textContent=processedWeather.date;
   currentPrimary.querySelector('.conditions').textContent=processedWeather.conditions;
   currentPrimary.querySelector('.desc').textContent=processedWeather.description;
-  currentPrimary.querySelector('.alerts').textContent=processedWeather.alerts;
+  if(processedWeather.alerts.length==0) currentPrimary.querySelector('.alerts').style.display='none';
   currentSecondary.querySelector('.feelslike .data').innerHTML=processedWeather.feelsLike+'&deg;c';
   currentSecondary.querySelector('.humidity .data').textContent=processedWeather.humidity+' %';
   currentSecondary.querySelector('.sunrise .data').textContent=processedWeather.sunrise;
   currentSecondary.querySelector('.sunset .data').textContent=processedWeather.sunset;
   let windUnit=document.createElement('span');
-  windUnit.textContent=' km/hr';
+  windUnit.textContent=' '+speedUnit;
   windUnit.style.fontSize='1rem';
   let windValue=document.createElement('span');
-  windValue.textContent=processedWeather.windSpeed
+  windValue.textContent=processedWeather.windSpeed;
   currentSecondary.querySelector('.windspeed .data').append(windValue,windUnit);
   currentSecondary.querySelector('.uvindex .data').textContent=processedWeather.uvIndex;
   currentSecondary.querySelector('.mintemp .data').innerHTML=processedWeather.mintemp+'&deg;c';
   currentSecondary.querySelector('.maxtemp .data').innerHTML=processedWeather.maxtemp+'&deg;c';
   currentSecondary.querySelector('.precipchance .data').textContent=processedWeather.precipchance+' %';
-  switch (processedWeather.icon) {
+  setConditionIcon(processedWeather.icon,currentPrimary.querySelector('.icon'));
+}
+
+function displayWeekWeather(processedWeather){
+  processedWeather.next7Days.forEach((day)=>{
+    const dayDiv = document.createElement('div');
+    dayDiv.classList.add('day');
+    const dateDiv=document.createElement('div');
+    dateDiv.classList.add('date');
+    let date=new Date(day.datetime);
+    dateDiv.textContent=date.getDate()+'/'+(date.getMonth()+1);
+    const img=document.createElement('img');
+    setConditionIcon(day.icon,img);
+    const temp=document.createElement('div');
+    temp.classList.add('temp');
+    temp.innerHTML=day.temp+'&deg;c';
+    const maxtemp=document.createElement('div');
+    maxtemp.classList.add('maxtemp');
+    maxtemp.textContent='Max: '+day.tempmax;
+    const mintemp=document.createElement('div');
+    mintemp.classList.add('mintemp');
+    mintemp.textContent='Min: '+day.tempmin;
+    const precip=document.createElement('div');
+    precip.classList.add('precipitation');
+    const icon=document.createElement('img');
+    icon.classList.add('icon');
+    icon.src=precipitationsvg;
+    const data=document.createElement('div');
+    data.classList.add('data');
+    data.textContent=day.precipprob;
+    precip.append(icon,data);
+    dayDiv.append(dateDiv,img,temp,maxtemp,mintemp,precip);
+    document.querySelector('.days').append(dayDiv);
+  })
+}
+
+function setConditionIcon(icon,imgElem){
+  switch (icon) {
     case "rain":
-      currentPrimary.querySelector('.icon').src=rainsvg;
+      imgElem.src=rainsvg;
       break;
-  
+    case "snow":
+      imgElem.src=snowsvg;
+      break;
+    case "fog":
+      imgElem.src=fogsvg;
+      break;
+    case "wind":
+      imgElem.src=windysvg;
+      break;
+    case "cloudy":
+      imgElem.src=cloudysvg;
+      break;
+    case "partly-cloudy-day":
+      imgElem.src=partlycloudydaysvg;
+      break;
+    case "partly-cloudy-night":
+      imgElem.src=partlycloudynightsvg;
+      break;
+    case "clear-day":
+      imgElem.src=cleardaysvg;
+      break;
+    case "clear-night":
+      imgElem.src=clearnightsvg;
+      break;
     default:
       break;
   }
